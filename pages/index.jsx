69ef091c6aaf9a88252966d4b831f2ed944b5901
hktari <bkamnik1995@gmail.com>
2024-09-02 11:32:01 +0200

    init next.js tailwind starter

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto bg-orange-200">
+      <h1 className="text-3xl">Next.js Google OAuth Example</h1>
+      <h3>by Boštjan Kamnik</h3>
+      <form action=""></form>
+    </div>
+  );
+}
